a6a182e98acd2373fba59f7255bfbc2bea78163a
HalilUcel <hucelsoft@gmail.com>
2022-03-12 23:15:03 +0300

    String extension is added new properys

--- lib/src/constant/regexp_constants.dart
+++ lib/src/constant/regexp_constants.dart
@@ -1,12 +1,17 @@
 class RegExpConstants {
   static RegExpConstants? _instance;
 
-  static RegExpConstants? get instance {
+  static RegExpConstants get instance {
     _instance ??= RegExpConstants._init();
-    return _instance;
+    return _instance!;
   }
 
   RegExpConstants._init();
 
-  final String emailRegexp = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$½'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+  final String emailRegexp =
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$½'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+  final String passwordRegexp = r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[!@#$\*~]).{8,}$";
+
+  //  final String passwordRegex =
+  //     r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
 }
--- lib/src/extension/string_extension.dart
+++ lib/src/extension/string_extension.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../constant/regexp_constants.dart';
 
@@ -18,8 +19,44 @@ extension StringValidator on String {
   bool get isNullOrEmpty => this == null || this.isEmpty;
   bool get isNotNullAndNotEmpty => !isNullOrEmpty;
 
-  bool get isValidEmail =>
-      RegExp(RegExpConstants.instance!.emailRegexp).hasMatch(this);
+  bool get isValidEmail => this != null
+      ? RegExp(RegExpConstants.instance.emailRegexp).hasMatch(this)
+      : false;
+  bool get isValidPassword => this != null
+      ? RegExp(RegExpConstants.instance.passwordRegexp).hasMatch(this)
+      : false;
+}
+
+extension AuthorizationExtension on String {
+  Map<String, dynamic> get beraer => {'Authırization': 'Bearer ${this}'};
+}
+
+extension LaunchExtension on String {
+  Future<bool> get launchEmail => launch('mailto:$this');
+  Future<bool> get launchPhone => launch('tel:$this');
+  Future<bool> get launchWebsite => launch(this);
+
+  Future<bool> launchWebsiteCuston({
+    bool? forceSafariVC,
+    bool forceWebView = false,
+    bool enableJavaScript = false,
+    bool enableDomStorage = false,
+    bool universalLinksOnly = false,
+    Map<String, String> headers = const <String, String>{},
+    Brightness? statusBarBrightness,
+    String? webOnlyWindowName,
+  }) =>
+      launch(
+        this,
+        forceSafariVC: forceSafariVC,
+        forceWebView: forceWebView,
+        enableDomStorage: enableDomStorage,
+        enableJavaScript: enableJavaScript,
+        universalLinksOnly: universalLinksOnly,
+        headers:  headers,
+        statusBarBrightness:  statusBarBrightness,
+        webOnlyWindowName: webOnlyWindowName
+      );
 }
 
 extension StringPathValidator on String {
