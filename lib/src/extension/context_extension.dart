93150c35d9ea84aefaba2d6c9d584dee6675c94d
Halil Ücel <96760811+HalilUcel14@users.noreply.github.com>
2022-03-16 16:19:31 +0300

    changed and upgraded code

--- lib/src/extension/context_extension.dart
+++ lib/src/extension/context_extension.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:best_baltanem/src/constant/responsivity_constants.dart';
 import 'package:flutter/material.dart';
 
 import 'int_extension.dart';
@@ -15,7 +16,7 @@ extension MediaQueryExtension on BuildContext {
   MediaQueryData get mediaQuery => MediaQuery.of(this);
   Size get size => mediaQuery.size;
   //
-  double get witdh => size.width;
+  double get width => size.width;
   double get height => size.height;
   //
   double dynamicHeight(double value) => height * value;
@@ -27,6 +28,13 @@ extension MediaQueryExtension on BuildContext {
   double get heightL => height * 0.06;
   double get heightXL => height * 0.08;
   double get heightXXL => height * 0.1;
+  //
+  double get widthXS => width * 0.01;
+  double get widthS => width * 0.02;
+  double get widthM => width * 0.04;
+  double get widthL => width * 0.06;
+  double get widthXL => width * 0.08;
+  double get widthXXL => width * 0.1;
   //
   bool get isKeyboardOpen => mediaQuery.viewInsets.bottom > 0;
   Brightness get appBrightness => mediaQuery.platformBrightness;
@@ -46,6 +54,34 @@ extension OrientationExtension on BuildContext {
 
 extension ColorExtension on BuildContext {
   MaterialColor get randomColor => Colors.primaries[17.randomValue];
+}
+
+// Device Screen Type By Width(300-600-900)
+// Values from https://flutter.dev/docs/development/ui/layout/building-adaptive-apps
+extension ContextDeviceTypeExtension on BuildContext {
+  bool get isSmallScreen =>
+      witdh < ResponsivityConstants.instance.smallScreenSize ? true : false;
+  bool get isNormalyScreen =>
+      witdh >= ResponsivityConstants.instance.smallScreenSize &&
+              witdh < ResponsivityConstants.instance.mediumScreenSize
+          ? true
+          : false;
+  bool get isMediumScreen =>
+      witdh >= ResponsivityConstants.instance.mediumScreenSize &&
+              witdh < ResponsivityConstants.instance.largeScreenSize
+          ? true
+          : false;
+  bool get isLargeScreen =>
+      witdh >= ResponsivityConstants.instance.largeScreenSize ? true : false;
+}
+
+extension DurationExtension on BuildContext {
+  Duration get durationXS => const Duration(milliseconds: 200);
+  Duration get durationS => const Duration(milliseconds: 300);
+  Duration get durationM => const Duration(milliseconds: 500);
+  Duration get durationL => const Duration(milliseconds: 800);
+  Duration get durationXL => const Duration(seconds: 1);
+  Duration get durationXXL => const Duration(seconds: 2);
 }
 
 extension PaddingExtension on BuildContext {
@@ -75,4 +111,8 @@ extension PaddingExtension on BuildContext {
   EdgeInsets get padVerticalXLarge => EdgeInsets.symmetric(vertical: heightXL);
   EdgeInsets get padVerticalXXLarge =>
       EdgeInsets.symmetric(vertical: heightXXL);
+}
+
+extension SizedBoxExtension on BuildContext{
+  Widget get emptySizedWidthBoxLow
 }
--- lib/src/widget/space_sized_widht_box.dart
+++ lib/src/widget/space_sized_widht_box.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 import '../../best_baltanem.dart';
 
-class SpaceSizedHeightBox extends StatelessWidget {
+class SpaceSizedWidthBox extends StatelessWidget {
   final double width;
-  const SpaceSizedHeightBox({Key? key, required this.width})
+  const SpaceSizedWidthBox({Key? key, required this.width})
       : assert(width > 0 && width <= 1),
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: context.witdh * width,
+      height: context.width * width,
     );
   }
 }
