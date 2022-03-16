30daccd752cfa29e9d66649238b988d2b46b54a0
Halil Ücel <96760811+HalilUcel14@users.noreply.github.com>
2022-03-16 19:16:17 +0300

    context extension is finish

--- lib/src/extension/context_extension.dart
+++ lib/src/extension/context_extension.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
 import 'package:best_baltanem/src/constant/responsivity_constants.dart';
+import 'package:best_baltanem/src/utility/page_animation/slider_route.dart';
+import 'package:best_baltanem/src/widget/space_sized_height_box.dart';
+import 'package:best_baltanem/src/widget/space_sized_widht_box.dart';
 import 'package:flutter/material.dart';
 
 import 'int_extension.dart';
@@ -20,7 +23,7 @@ extension MediaQueryExtension on BuildContext {
   double get height => size.height;
   //
   double dynamicHeight(double value) => height * value;
-  double dynamicWidth(double value) => witdh * value;
+  double dynamicWidth(double value) => width * value;
   //
   double get heightXS => height * 0.01;
   double get heightS => height * 0.02;
@@ -60,19 +63,19 @@ extension ColorExtension on BuildContext {
 // Values from https://flutter.dev/docs/development/ui/layout/building-adaptive-apps
 extension ContextDeviceTypeExtension on BuildContext {
   bool get isSmallScreen =>
-      witdh < ResponsivityConstants.instance.smallScreenSize ? true : false;
+      width < ResponsivityConstants.instance.smallScreenSize ? true : false;
   bool get isNormalyScreen =>
-      witdh >= ResponsivityConstants.instance.smallScreenSize &&
-              witdh < ResponsivityConstants.instance.mediumScreenSize
+      width >= ResponsivityConstants.instance.smallScreenSize &&
+              width < ResponsivityConstants.instance.mediumScreenSize
           ? true
           : false;
   bool get isMediumScreen =>
-      witdh >= ResponsivityConstants.instance.mediumScreenSize &&
-              witdh < ResponsivityConstants.instance.largeScreenSize
+      width >= ResponsivityConstants.instance.mediumScreenSize &&
+              width < ResponsivityConstants.instance.largeScreenSize
           ? true
           : false;
   bool get isLargeScreen =>
-      witdh >= ResponsivityConstants.instance.largeScreenSize ? true : false;
+      width >= ResponsivityConstants.instance.largeScreenSize ? true : false;
 }
 
 extension DurationExtension on BuildContext {
@@ -113,6 +116,62 @@ extension PaddingExtension on BuildContext {
       EdgeInsets.symmetric(vertical: heightXXL);
 }
 
-extension SizedBoxExtension on BuildContext{
-  Widget get emptySizedWidthBoxLow
+extension SizedBoxExtension on BuildContext {
+  Widget get emptySizedBoxWidthXS => SpaceSizedWidthBox(width: widthXS);
+  Widget get emptySizedBoxWidthS => SpaceSizedWidthBox(width: widthS);
+  Widget get emptySizedBoxWidthM => SpaceSizedWidthBox(width: widthM);
+  Widget get emptySizedBoxWidthL => SpaceSizedWidthBox(width: widthL);
+  Widget get emptySizedBoxWidthXL => SpaceSizedWidthBox(width: widthXL);
+  Widget get emptySizedBoxWidthXXL => SpaceSizedWidthBox(width: widthXXL);
+  //
+  Widget get emptySizedBoxHeightXS => SpaceSizedHeightBox(height: heightXS);
+  Widget get emptySizedBoxHeightS => SpaceSizedHeightBox(height: heightS);
+  Widget get emptySizedBoxHeightM => SpaceSizedHeightBox(height: heightM);
+  Widget get emptySizedBoxHeightL => SpaceSizedHeightBox(height: heightL);
+  Widget get emptySizedBoxHeightXL => SpaceSizedHeightBox(height: heightXL);
+  Widget get emptySizedBoxHeightXXL => SpaceSizedHeightBox(height: heightXXL);
+}
+
+extension RadiusExtension on BuildContext {
+  Radius get radiusXS => Radius.circular(widthXS);
+  Radius get radiusS => Radius.circular(widthS);
+  Radius get radiusM => Radius.circular(widthM);
+  Radius get radiusL => Radius.circular(widthL);
+  Radius get radiusXL => Radius.circular(widthXL);
+  Radius get radiusXXL => Radius.circular(widthXXL);
+}
+
+extension BorderExtension on BuildContext {
+  BorderRadius get borderRadiusXS => BorderRadius.all(Radius.circular(widthXS));
+  BorderRadius get borderRadiusS => BorderRadius.all(Radius.circular(widthS));
+  BorderRadius get borderRadiusM => BorderRadius.all(Radius.circular(widthM));
+  BorderRadius get borderRadiusL => BorderRadius.all(Radius.circular(widthL));
+  BorderRadius get borderRadiusXL => BorderRadius.all(Radius.circular(widthXL));
+  BorderRadius get borderRadiusXXL =>
+      BorderRadius.all(Radius.circular(widthXXL));
+  //
+
+}
+
+extension NavigationExtension on BuildContext {
+  NavigatorState get navigation => Navigator.of(this);
+
+  Future<T?> pop<T>([T? data]) async {
+    await navigation.maybePop(data);
+  }
+
+  Future<T?> navigateName<T>(String path, {Object? data}) async {
+    return await navigation.pushNamed<T>(path, arguments: data);
+  }
+
+  Future<T?> navigateToReset<T>(String path, {Object? data}) async {
+    return await navigation.pushNamedAndRemoveUntil(path, (route) => false,
+        arguments: data);
+  }
+
+  Future<T?> navigateToPage<T>(Widget page,
+      {Object? extra, SlideType type = SlideType.defauld}) async {
+    return await navigation
+        .push(type.route(page, RouteSettings(arguments: extra)));
+  }
 }
