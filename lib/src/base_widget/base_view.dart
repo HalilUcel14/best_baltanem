e2a54694718c24fcba374a586aa87347c9273bf0
HalilUcel <hucelsoft@gmail.com>
2022-03-26 22:31:01 +0300

    add mobx state

---
+++ lib/src/base_widget/base_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+
+import 'base_statefull.dart';
+
+class BaseView<T extends Store> extends StatefulWidget {
+  const BaseView(
+      {Key? key,
+      required this.viewModel,
+      this.onModelReady,
+      this.onDispose,
+      required this.onPageBuilder})
+      : super(key: key);
+
+  final T viewModel;
+  final Function(T model)? onModelReady;
+  final VoidCallback? onDispose;
+  final Widget Function(BuildContext context, T value) onPageBuilder;
+
+  @override
+  _BaseViewState createState() => _BaseViewState();
+}
+
+class _BaseViewState extends BaseStateful<BaseView> {
+  @override
+  void initState() {
+    super.initState();
+    // widget.onModelReady != null ? widget.onModelReady!(widget.viewModel) : null;
+    widget.onModelReady ?? widget.onModelReady!(widget.viewModel);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    //widget.onDispose != null ? widget.onDispose! : null;
+    widget.onDispose ?? widget.onDispose;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.onPageBuilder(context, widget.viewModel);
+  }
+}
